f51468169a51fc5b4880a83170fba4078f3433c0
Guilherme Nono <guilherme.nono45789@gmail.com>
2023-02-04 10:00:31 -0300

    First Commit
    - Backend base structure

---
+++ src/main/java/br/com/projlib/bookshelf/BookshelfApplication.java
@@ -0,0 +1,13 @@
+package br.com.projlib.bookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshelfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookshelfApplication.class, args);
+    }
+
+}
