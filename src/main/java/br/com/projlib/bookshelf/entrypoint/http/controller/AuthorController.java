a40fe0cd22398a814dbf05a0d99b1bd25bc89ab6
Gabriel Morais <gabe-morais@outlook.com>
2023-06-17 20:04:44 -0300

    GetMapping e Operation

--- src/main/java/br/com/projlib/bookshelf/entrypoint/http/controller/AuthorController.java
+++ src/main/java/br/com/projlib/bookshelf/entrypoint/http/controller/AuthorController.java
@@ -2,12 +2,16 @@ package br.com.projlib.bookshelf.entrypoint.http.controller;
 
 import br.com.projlib.bookshelf.core.usecase.FindAllAuthor;
 import br.com.projlib.bookshelf.entrypoint.http.response.AuthorResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +27,9 @@ public class AuthorController {
     private final FindAllAuthor findAllAuthor;
 
     private final ModelMapper modelMapper;
-
+    @Operation(summary = "Get Authors")
+    @GetMapping
+    @SecurityRequirement(name = "Bearer Authentication")
     public ResponseEntity<List<AuthorResponse>> getAll() {
         try{
             List<AuthorResponse> responses = findAllAuthor.process().stream()
